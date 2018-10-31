f47b2631fcdd8254dbffc1618cdd02245ad443b5
erichu801 <eric.hu801@gmail.com>
2018-10-31 13:50:58 -0700

    local video

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
-  
+	// Local Video
+	const localVideoEl = $('#vid');
+
+	const webrtc = new SimpleWebRTC({
+		// the id/element dom element that will hold "our" video
+		localVideoEl: 'vid',
+		// immediately ask for camera access
+		autoRequestMedia: true,
+	});
+	// We got access to local camera
+	webrtc.on('localStream', () => {
+		localVideoEl.show();
+	});
 });
