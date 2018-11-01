fac8303b63bfae389696ef58470e3995ea54a5b9
erichu801 <eric.hu801@gmail.com>
2018-11-01 16:15:04 -0700

    trying to fix everything

--- public/js/script.js
+++ public/js/script.js
@@ -2,33 +2,14 @@ window.addEventListener('load', () => {
 	// Local Video
 	const localVidObj = $('#localVid');
 
-	// Remote Videos
-	const remoteVidObj = $('#remoteVid');
-	let linksCount = 0;
-
 	const webrtc = new SimpleWebRTC({
 		// the id/element dom element that will hold "our" video
 		remoteVidObj: 'localVid',
-		// the id/element dom element that will hold remote videos
-		remoteVidObj: 'remoteVid',
 		// immediately ask for camera access
 		autoRequestMedia: true,
 	});
 	// We got access to local camera
 	webrtc.on('localStream', () => {
 		localVidObj.show();
-	});
-
-	// Remote video was added
-	webrtc.on('videoAdded', (video, peer) => {
-		const id = webrtc.getDomId(peer);
-		const html = remoteVideoTemplate({ id });
-		if (linksCount == 0) {
-			remoteVidObj.html(html);
-		} else {
-			remoteVidObj.append(html);
-		}
-		$(`#${id}`).html(video);
-		linksCount ++;
 	});
 });
