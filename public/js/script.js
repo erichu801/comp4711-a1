15038f61ae39d48447a64460fd7b299e2d146c71
erichu801 <eric.hu801@gmail.com>
2018-11-01 11:30:08 -0700

    removed firebase stuff, added templates for remote video

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', () => {
 	// Local Video
-	const localVideoEl = $('#vid');
+	const localVideoEl = $('#localVid');
+
+	// Remote Videos
+	const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html());
+	const remoteVideosEl = $('#remoteVid');
+	let remoteVideosCount = 0;
 
 	const webrtc = new SimpleWebRTC({
 		// the id/element dom element that will hold "our" video
-		localVideoEl: 'vid',
+		localVideoEl: 'localVid',
+		// the id/element dom element that will hold remote videos
+		remoteVideosEl: 'remoteVid',
 		// immediately ask for camera access
 		autoRequestMedia: true,
 	});
