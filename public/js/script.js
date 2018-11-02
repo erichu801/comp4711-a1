b6fb38b1d0f09a77a387e1b7ef027e3996b9c883
erichu801 <eric.hu801@gmail.com>
2018-11-01 17:05:23 -0700

    remote vid

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,14 @@ window.addEventListener('load', () => {
 	// Local Video
 	const localVideoEl = $('#localVid');
 
+	const remoteVideosEl = $('#remoteVids');
+
 	// create our WebRTC connection
 	const webrtc = new SimpleWebRTC({
 		// the id/element dom element that will hold "our" video
 		localVideoEl: 'localVid',
+		// the id/element dom element that will hold remote videos
+		remoteVideosEl: 'remoteVids',
 		// immediately ask for camera access
 		autoRequestMedia: true,
 	});
