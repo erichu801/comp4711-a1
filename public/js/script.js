0b186843c13e71113372238920109b177d0cfd64
erichu801 <eric.hu801@gmail.com>
2018-11-03 15:32:39 -0700

    display username

--- public/js/script.js
+++ public/js/script.js
@@ -1,66 +1,70 @@
 window.addEventListener('load', () => {
 
-	const formEl = $('.form');
+	const form = $('.form');
 	let displayName;
 
-	// Local Video
 	const localVideoEl = $('#localVid');
 
 	const remoteVideosEl = $('#remoteVids');
-	let extUsrCount = 0;
+	let numRemotes = 0;
 
-	// create our WebRTC connection
 	const webrtc = new SimpleWebRTC({
-		// the id/element dom element that will hold "our" video
 		localVideoEl: 'localVid',
-		// the id/element dom element that will hold remote videos
 		remoteVideosEl: 'remoteVids',
-		// immediately ask for camera access
 		autoRequestMedia: true,
 	});
 
-	// We got access to local camera
 	webrtc.on('localStream', () => {
 		localVideoEl.show();
 	});
  
 
-	$('.submit').on('click', (event) => {
+	$('#btnCreate').on('click', () => {
 		displayName = $('#displayName').val();
 		const roomKey = $('#roomKey').val().toLowerCase();
-		if (event.target.id === 'create-btn') {
-		  	createRoom(roomKey);
-		} else {
-			joinRoom(roomKey);
-		}
-		return false;
+		createRoom(roomKey);
 	});
 
-	// Register new Chat Room
-	const createRoom = (roomKey) => {
-		console.info(`Creating new room: ${roomKey}`);
-		webrtc.createRoom(roomKey, (err, name) => {
+	$('#btnJoin').on('click', () => {
+		displayName = $('#displayName').val();
+		const roomKey = $('#roomKey').val().toLowerCase();
+		joinRoom(roomKey);
+		
+	});
 
+	const createRoom = (roomKey) => {
+		webrtc.createRoom(roomKey, (err, name) => {
+			form.hide();
+			postMessage(displayName + ` created chatroom`);
 		});
 	};
 
-	// Join existing Chat Room
 	const joinRoom = (roomKey) => {
-		console.log(`Joining Room: ${roomKey}`);
 		webrtc.joinRoom(roomKey);
+		form.hide();
+		postMessage(displayName + ` joined chatroom`);
 	};
 
-	// Remote video was added
 	webrtc.on('videoAdded', (video, peer) => {
-		console.log("here");
 		const id = webrtc.getDomId(peer);
-		if (extUsrCount === 0) {
-			console.log("test1");
+		if (numRemotes === 0) {
 			remoteVideosEl.append('<video id=' + id + '></video>');
 		} else {
-			console.log("test2");
 			remoteVideosEl.append('<video id=' + id + '></video>');
 		}
-		extUsrCount++;
+		numRemotes++;
+	});
+
+	let postMessage = (data) => {
+		console.log(displayName);
+		webrtc.sendToAll('chat', {message: displayName});
+	};
+	
+	webrtc.connection.on('message', (data) => {
+		console.log(data.type);
+		if(data.type === 'chat'){
+			console.log('chat received', data);
+			console.log('<br><br>' + data.payload.message);
+		}
 	});
 });
