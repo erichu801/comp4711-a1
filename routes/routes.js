035942ad6d1670b92149c04ea23a229878b248e1
erichu801 <eric.hu801@gmail.com>
2018-11-14 14:57:13 -0800

    implemented uuid

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,20 @@
+const uuidv4 = require('uuid/v4');
+
 const appRouter = (app) => {
     app.get("/api/", function(req, res) {
-        const key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        const key = uuidv4();
         data = ({
             roomKey: key,
-            url: "localhost:8080/?roomKey=" + key
+            url: "/?roomKey=" + key
         });
         res.status(200).send(data);
     });
 
     app.post("/api/", function(req, res) {
-        let urlString = window.location.href;
-        let url = new URL(urlString);
-        const key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        const key = uuidv4();
         data = ({
             roomKey: key,
-            url: url + "/?roomKey=" + key
+            url: "/?roomKey=" + key
         });
         res.status(200).send(data);
     });
