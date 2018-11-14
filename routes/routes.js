2dbbeada1ac35832da04cb01ff9df4f6cd0797b3
erichu801 <eric.hu801@gmail.com>
2018-11-14 14:46:12 -0800

    post request

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,15 @@ const appRouter = (app) => {
         });
         res.status(200).send(data);
     });
+
+    app.post("/api/", function(req, res) {
+        const key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        data = ({
+            roomKey: key,
+            url: "localhost:8080/?roomKey=" + key
+        });
+        res.status(200).send(data);
+    });
 }
   
 module.exports = appRouter;
