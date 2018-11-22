16874af4f7928c053ea66843143438c5600b4ddf
erichu801 <eric.hu801@gmail.com>
2018-11-22 11:17:12 -0800

    bb

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 
 let port = process.env.PORT || 8080;
 
-
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
