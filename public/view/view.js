a410b54709f109786dbbe12e374d94552c1c1eed
lilywang888 <lilywang888@gmail.com>
2018-11-29 21:37:13 -0800

    change back to original -Li

--- public/view/view.js
+++ public/view/view.js
@@ -22,22 +22,12 @@ const genUI = () => {
 			<h4 id="localId">You</h4>
 			<video id="localVid" width="320" height="240" autoplay></video>
 		</div>
-		<div id="video_row" class="row">
-			<div class="col-sm-1">
-				<div id="remoteVids1" class="col-sm"></div>
+		<div class="row">
+			<div class="col-sm">
+				<div id="remoteVids" class="col-sm"></div>
 			</div>
-			<div class="col-sm-1">
-				<div id="remoteVids2" class="col-sm"></div>
-			</div>
-			<div class="col-sm-1">
-				<div id="remoteVids3" class="col-sm"></div>
-			</div>
-			<div class="col-sm-1">
-				<div id="remoteVids4" class="col-sm"></div>
-			</div>
-			<div class="col-sm-1">
-				<div id="remoteVids5" class="col-sm"></div>
-			</div>
+			<div class="col-sm"></div>
+			<div class="col-sm"></div>
 		</div>
 	`);
 };
