327e4b0ed78d940bfcbcea1c336d7edfed6acb91
lilywang888 <lilywang888@gmail.com>
2018-11-29 16:20:21 -0800

    clone form herohu and modified view and controller

--- public/view/view.js
+++ public/view/view.js
@@ -1,5 +1,3 @@
-
-
 //Generate the User Interface
 const genUI = () => {
 	$('.container').html(`
@@ -9,30 +7,37 @@ const genUI = () => {
 					<label>Display Name</label>
 					<input type="text" class="form-control input-sm" id="displayName" placeholder="Enter display name" name="displayName">
 					` +
+					//<label>Room Key</label>
+					//<input type="text" class="form-control input-sm" id="roomKey" placeholder="Enter room key" name="roomKey">
 					`<br>
 					` +
-					`
-					<form action="?" method="POST">
-						<div id="myCaptcha"></div>
-					</form>
-					<br>
-					<button id="btnJoin" class="btn btn-info">Join Room</button>					
+					//<button id="btnCreate" class="btn btn-success">Create Room</button>
+					`<button id="btnJoin" class="btn btn-info">Join Room</button>					
 				</div>
 			</div>
 			<div class="col-sm"></div>
-				
 			<div class="col-sm"></div>
 		</div>
 		<div>
 			<h4 id="localId">You</h4>
 			<video id="localVid" width="320" height="240" autoplay></video>
 		</div>
-		<div class="row">
-			<div class="col-sm">
-				<div id="remoteVids" class="col-sm"></div>
+		<div id="video_row" class="row">
+			<div class="col-sm-1">
+				<div id="remoteVids1" class="col-sm"></div>
 			</div>
-			<div class="col-sm"></div>
-			<div class="col-sm"></div>
+			<div class="col-sm-1">
+				<div id="remoteVids2" class="col-sm"></div>
+			</div>
+			<div class="col-sm-1">
+				<div id="remoteVids3" class="col-sm"></div>
+			</div>
+			<div class="col-sm-1">
+				<div id="remoteVids4" class="col-sm"></div>
+			</div>
+			<div class="col-sm-1">
+				<div id="remoteVids5" class="col-sm"></div>
+			</div>
 		</div>
 	`);
 };
