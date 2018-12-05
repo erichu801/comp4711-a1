e0f2232d72b39e5384457312a2be0e759e69d436
erichu801 <eric.hu801@gmail.com>
2018-12-04 17:20:20 -0800

    user list

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -92,7 +92,7 @@ window.addEventListener('load', () => {
 		if(data.type === 'chat') {
 			remoteNames = [];
             remoteNames.push(data.payload.name);
-            console.log(remoteNames);
+            $('#user-list').append(remoteNames);
 			for(let i = 0; i < remoteNames.length; i += data.payload.index) {
 				$('#h' + data.payload.index).html(data.payload.name);
 			}
