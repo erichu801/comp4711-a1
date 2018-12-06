0c01576dc22ed2de00cf32382db309c32c3536b2
erichu801 <eric.hu801@gmail.com>
2018-12-05 20:33:24 -0800

    api

--- routes/routes.js
+++ routes/routes.js
@@ -2,43 +2,63 @@ const uuidv4 = require('uuid/v4');
 
 const appRouter = (app) => {
     app.get("/api/", function(req, res) {
-        let token = req.headers["token"];
-        const key = uuidv4();
-        if(!token.localeCompare("badgebook")) {
-            data = ({
-                received_token: token,
-                roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
-            });
+        if(req.headers["token"] != null) {
+            let token = req.headers["token"];
+            const key = uuidv4();
+            if(!token.localeCompare("badgebook")) {
+                data = ({
+                    received_token: token,
+                    roomKey: key,
+                    url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
+                });
+            } else {
+                data = ({
+                    received_token: token,
+                    roomKey: key,
+                    url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
+                });
+            }
+            
+            res.status(200).send(data);
         } else {
+            const key = uuidv4();
             data = ({
                 received_token: token,
                 roomKey: key,
                 url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
             });
+            res.status(200).send(data);
         }
-        
-        res.status(200).send(data);
     });
 
     app.post("/api/", function(req, res) {
-        let token = req.headers["token"];
-        const key = uuidv4();
-        if(!token.localeCompare("badgebook")) {
-            data = ({
-                received_token: token,
-                roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
-            });
+        if(req.headers["token"] != null) {
+            let token = req.headers["token"];
+            const key = uuidv4();
+            if(!token.localeCompare("badgebook")) {
+                data = ({
+                    received_token: token,
+                    roomKey: key,
+                    url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
+                });
+            } else {
+                data = ({
+                    received_token: token,
+                    roomKey: key,
+                    url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
+                });
+            }
+            
+            res.status(200).send(data);
         } else {
+            const key = uuidv4();
             data = ({
                 received_token: token,
                 roomKey: key,
                 url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
             });
+            res.status(200).send(data);
         }
-        
-        res.status(200).send(data);
     });
 }
   
