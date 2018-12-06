c1ca7df6ea62e88edee35e629eeedce8a3d2846a
erichu801 <eric.hu801@gmail.com>
2018-12-05 20:41:53 -0800

    api

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,14 @@ const uuidv4 = require('uuid/v4');
 
 const appRouter = (app) => {
     app.get("/api/", function(req, res) {
+        const key = uuidv4();
+        data = ({
+            received_token: token,
+            roomKey: key,
+            url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
+        });
+        res.status(200).send(data);
+
         if(req.headers["token"] != null) {
             let token = req.headers["token"];
             const key = uuidv4();
@@ -20,18 +28,18 @@ const appRouter = (app) => {
             }
             
             res.status(200).send(data);
-        } else {
-            const key = uuidv4();
-            data = ({
-                received_token: token,
-                roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
-            });
-            res.status(200).send(data);
-        }
+        }        
     });
 
     app.post("/api/", function(req, res) {
+        const key = uuidv4();
+        data = ({
+            received_token: token,
+            roomKey: key,
+            url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
+        });
+        res.status(200).send(data);
+        
         if(req.headers["token"] != null) {
             let token = req.headers["token"];
             const key = uuidv4();
@@ -50,15 +58,7 @@ const appRouter = (app) => {
             }
             
             res.status(200).send(data);
-        } else {
-            const key = uuidv4();
-            data = ({
-                received_token: token,
-                roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
-            });
-            res.status(200).send(data);
-        }
+        }    
     });
 }
   
