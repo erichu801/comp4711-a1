7e3a53a243477a29bbeafccdda6718a968febfd7
erichu801 <eric.hu801@gmail.com>
2018-12-05 21:38:30 -0800

    api revert

--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,43 @@
 const uuidv4 = require('uuid/v4');
+let reqCaptcha = false;
 
 const appRouter = (app) => {
     app.get("/api/", function(req, res) {
         let token = req.headers["token"];
+        if(token != "badgebook") {
+            reqCaptcha = true;
+        }
         const key = uuidv4();
-        if(!token.localeCompare("badgebook")) {
+        if(reqCaptcha) {
             data = ({
-                received_token: token,
-                roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
-            });
-        } else {
-            data = ({
-                received_token: token,
                 roomKey: key,
                 url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
             });
+        } else {
+            data = ({
+                roomKey: key,
+                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
+            });
         }
         
-        res.status(200).send(data);     
+        res.status(200).send(data);
     });
 
     app.post("/api/", function(req, res) {
         let token = req.headers["token"];
+        if(token != "badgebook") {
+            reqCaptcha = true;
+        }
         const key = uuidv4();
-        if(!token.localeCompare("badgebook")) {
+        if(reqCaptcha) {
             data = ({
-                received_token: token,
                 roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
+                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
             });
         } else {
             data = ({
-                received_token: token,
                 roomKey: key,
-                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=1"
+                url: "https://comp4711-a1.herokuapp.com/?roomKey=" + key + "&captcha=0"
             });
         }
         
